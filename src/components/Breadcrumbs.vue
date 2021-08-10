c913de2c6ea72aa8dbd58b3065ada044de99df33
Paulo dos Santos <paulo@motovalor.com>
2021-08-10 13:35:27 -0300

    current state

--- src/components/Breadcrumbs.vue
+++ src/components/Breadcrumbs.vue
@@ -1,31 +1,28 @@
 <template>
   <nav class="flex" aria-label="Breadcrumb">
     <ol class="bg-white rounded-md shadow px-6 flex space-x-4">
+      <li v-if="breadcrumbs.length" class="flex">
+        <div class="flex items-center">
+          <router-link :to="{ name: breadcrumbs[0].name, params: breadcrumbs[0].params }" class="text-gray-400 hover:text-gray-500">
+            <HomeIcon class="flex-shrink-0 h-5 w-5" aria-hidden="true" />
+            <span class="sr-only">Home</span>
+          </router-link>
+        </div>
+      </li>
       <li
-        v-for="(breadcrumb, i) in breadcrumbs"
+        v-for="breadcrumb in breadcrumbs"
         :key="breadcrumb.name"
         class="flex">
-        <div class="flex items-center h-full">
+        <div
+          class="flex items-center">
+          <svg class="flex-shrink-0 w-6 h-full text-gray-200" viewBox="0 0 24 44" preserveAspectRatio="none" fill="currentColor" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+            <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
+          </svg>
           <router-link
-            v-if="i === 0"
-            :to="{ name: breadcrumb.name }"
-            class="flex items-center text-gray-400 hover:text-gray-500">
-            <HomeIcon class="flex-shrink-0 h-5 w-5" aria-hidden="true" />
-            <span class="ml-1 text-sm font-medium text-gray-500 hover:text-gray-700">
-              {{breadcrumb.name}}
-            </span>
+            :to="{ name: breadcrumb.name, params: breadcrumb.params }"
+            class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700">
+            {{breadcrumb.name}}
           </router-link>
-          <template v-else>
-            <svg class="flex-shrink-0 w-6 h-full text-gray-200" viewBox="0 0 24 44" preserveAspectRatio="none" fill="currentColor" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-              <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
-            </svg>
-            <router-link
-              :to="{ name: breadcrumb.name, params: breadcrumb.params }"
-              class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700">
-              {{breadcrumb.name}}
-            </router-link>
-          </template>
-            <!--<a :href="page.href" class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700" :aria-current="page.current ? 'page' : undefined">{{ page.name }}</a>-->
         </div>
       </li>
     </ol>
